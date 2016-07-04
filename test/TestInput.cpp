bf3412cbde7b9f2a5266a6c1bc044adb406e2dcb
JOG-CLT <ortegagingrich@gmail.com>
2016-07-04 12:31:44 -0700

    Added test for overlapping event priorities.

--- test/TestInput.cpp
+++ test/TestInput.cpp
@@ -135,8 +135,170 @@ TEST(Input, MultipleButtonCallbacks){
 	 * Cleanup
 	 */
 	delete window; // Automatically takes care of callbacks
-	
 }
+
+
+
+TEST(Input, EventPriority){
+	/**
+	 * This test is to ensure that callbacks are given their expected priorities
+	 * when handling events which match multiple ones.
+	 * 
+	 * Priorities are designed as follows:
+	 * 1) If two callbacks are in different layers, the one in the higher layer
+	 *    gets priority.
+	 * 2) Within a layer, callbacks in the scene graph receive priority over
+	 *    those attached to the layer itself.
+	 * 3) Callbacks attached to the window receive lowest priority.
+	 * 4) Callbacks both in the same scene graph, both attached directly to the
+	 *    same layer or both attached to the same window have the have the
+	 *    same priority.  (TODO: explicit priorities?)
+	 * 5) A callback with higher priority will always be called before a 
+	 *    callback with lower priority.
+	 * 6) The order in which callbacks of the same priority are called should
+	 *    be considered non-deterministic.
+	 */
+	
+	/*
+	 * Callback definition
+	 */
+	
+	class PriorityCallback : public KeyButtonCallback {
+	public:
+		bool activated;
+		
+		PriorityCallback(JWindow *win): KeyButtonCallback(win), activated(false){};
+		PriorityCallback(Layer *l): KeyButtonCallback(l), activated(false){};
+		PriorityCallback(ComponentInput2D *c): KeyButtonCallback(c), activated(false){};
+		
+		void reset(){activated = false;};
+		
+		virtual void callback(KeyButtonEvent *event){
+			activated = true;
+			event->consume();
+		};
+		
+		// If this method is called for any reason, automatic failure
+		virtual void callback(InputEvent *event){
+			FAIL();
+		};
+	};
+	
+	/*
+	 * Setup
+	 */
+	
+	JWindow *window = new JWindow(100, 100, "TEST_INPUT");
+	
+	Layer2D *layer1, *layer2;
+	NodeInput2D *node;
+	
+	layer1 = new Layer2D("Layer 1");
+	layer2 = new Layer2D("Layer 2");
+	node = new NodeInput2D();
+	
+	layer2->rootNode->attachChild(node);
+	window->addLayerBottom(layer2);
+	window->addLayerTop(layer1);
+	
+	SDL_Event sdlEvent;
+	sdlEvent.type = SDL_KEYDOWN;
+	
+	
+	/*
+	 * Add callbacks one at a time, from expected lowest priority to highest.
+	 * Each time, process the SDL event to make sure that only the callback with
+	 * the highest priority is activated.
+	 */
+	
+	// Trial 1
+	
+	PriorityCallback *c1 = new PriorityCallback(window);
+	window->processEvent(sdlEvent);
+	
+	EXPECT_EQ(c1->activated, true);
+	
+	c1->reset();
+	
+	// Trial 2
+	
+	PriorityCallback *c2 = new PriorityCallback(layer2);
+	window->processEvent(sdlEvent);
+	
+	EXPECT_EQ(c1->activated, false);
+	EXPECT_EQ(c2->activated, true);
+	
+	c1->reset();
+	c2->reset();
+	
+	// Trial 3
+	
+	PriorityCallback *c3 = new PriorityCallback(node);
+	window->processEvent(sdlEvent);
+	
+	EXPECT_EQ(c1->activated, false);
+	EXPECT_EQ(c2->activated, false);
+	EXPECT_EQ(c3->activated, true);
+	
+	c1->reset();
+	c2->reset();
+	c3->reset();
+	
+	// Trial 4
+	
+	PriorityCallback *c4 = new PriorityCallback(layer1);
+	window->processEvent(sdlEvent);
+	
+	EXPECT_EQ(c1->activated, false);
+	EXPECT_EQ(c2->activated, false);
+	EXPECT_EQ(c3->activated, false);
+	EXPECT_EQ(c4->activated, true);
+	
+	c1->reset();
+	c2->reset();
+	c3->reset();
+	c4->reset();
+	
+	
+	/*
+	 * Cleanup
+	 */
+	delete window;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
