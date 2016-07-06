996715c1eb7aa3f67d38760ea003010937d5d7fb
JOG-CLT <ortegagingrich@gmail.com>
2016-07-06 16:54:35 -0700

    Skeleton for some basic renderables (points and lines)

--- src/jvisu/renderable.cpp
+++ src/jvisu/renderable.cpp
@@ -1,4 +1,74 @@
+#include <cstdio>
+
+#include "sdl.h"
 #include "renderable.h"
+#include "window.h"
+
+
+
+/*
+ * RenderableLine
+ */
+
+RenderableLine::RenderableLine(float xi1, float yi1, float xi2, float yi2, float z,
+                               float w, Uint8 cr, Uint8 cg, Uint8 cb, Uint8 ca):
+	Renderable(z),
+	x1(xi1), x2(xi2), y1(yi1), y2(yi2),
+	width(w),
+	colorRed(cr),
+	colorGreen(cg),
+	colorBlue(cb),
+	colorAlpha(ca)
+{}
+
+
+void RenderableLine::render(SDL_Renderer *renderer, JWindow *window){
+	// Compute screen coordinates
+	int px1, py1, px2, py2;
+	window->viewportToScreen(x1, y1, px1, py1);
+	window->viewportToScreen(x2, y2, px2, py2);
+	
+	// Draw Color
+	SDL_SetRenderDrawColor(renderer, colorRed, colorGreen, colorBlue, colorAlpha);
+	
+	// TODO: Actually implement this part!!!
+	printf("Pretending to draw line of width %f from (%f, %f) to (%f, %f) . . .",
+	       width, x1, y1, x2, y2);
+	printf("Actual Coordiantes: (%d, %d) -> (%d, %d)\n", px1, py1, px2, py2);
+}
+
+
+
+/*
+ * RenderablePoint
+ */
+
+RenderablePoint::RenderablePoint(float x, float y, float z, float r,
+                                Uint8 cr, Uint8 cg, Uint8 cb, Uint8 ca):
+	Renderable(z),
+	xPosition(x),
+	yPosition(y),
+	radius(r),
+	colorRed(cr),
+	colorGreen(cg),
+	colorBlue(cb),
+	colorAlpha(ca)
+{}
+
+
+void RenderablePoint::render(SDL_Renderer *renderer, JWindow *window){
+	// First, compute the pixel coordinates of the point center
+	int pixelX, pixelY;
+	window->viewportToScreen(xPosition, yPosition, pixelX, pixelY);
+	
+	// Set render draw color
+	SDL_SetRenderDrawColor(renderer, colorRed, colorGreen, colorBlue, colorAlpha);
+	
+	// TODO: Actually implement this part!!!
+	printf("Pretending to draw point of radius %f at (%f, %f, %f) . . . ",
+	       radius, xPosition, yPosition, zLevel);
+	printf("Actual Coordinates: (%d, %d)\n", pixelX, pixelY);
+}
 
 
 
--- src/jvisu/renderable.h
+++ src/jvisu/renderable.h
@@ -2,6 +2,10 @@
  * Definitions and Declarations for renderable objects.
  * These are small lightweight objects which are created by some Layer sub-types
  * at render time.
+ * 
+ * Note that all x and y coordinates used by renderables are viewport (float)
+ * coordinates.  These define (0,0) as the center of the screen.  The positive
+ * x-direction is right and the positive y-direction is up.
  */
 #ifndef RENDERABLE_H
 #define RENDERABLE_H
@@ -19,8 +23,41 @@ class Renderable {
 public:
 	const float zLevel;
 	Renderable(float z): zLevel(z) {};
-	virtual ~Renderable();
+	virtual ~Renderable(){};
 	virtual void render(SDL_Renderer *renderer, JWindow *window) = 0;
+};
+
+
+class RenderableLine : public Renderable {
+	/**
+	 * Class of renderable lines
+	 */
+public:
+	const float x1, x2, y1, y2;
+	const float width;
+	const Uint8 colorRed, colorGreen, colorBlue, colorAlpha;
+	
+	RenderableLine(float xi1, float yi1, float xi2, float yi2, float z, float w,
+	               Uint8 cr, Uint8 cg, Uint8 cb, Uint8 ca);
+	
+	virtual void render(SDL_Renderer *renderer, JWindow *window);
+};
+
+
+class RenderablePoint : public Renderable {
+	/**
+	 * Class of renderable points
+	 */
+public:
+	const float xPosition, yPosition;
+	const float radius;
+	const Uint8 colorRed, colorGreen, colorBlue, colorAlpha;
+	
+	
+	RenderablePoint(float x, float y, float z, float r,
+	                Uint8 cr, Uint8 cg, Uint8 cb, Uint8 ca);
+	
+	virtual void render(SDL_Renderer *renderer, JWindow *window);
 };
 
 
